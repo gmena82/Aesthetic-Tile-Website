0a730301ffefc050805b3174824584b2b8915cda
gmena82 <130086132+gmena82@users.noreply.github.com>
2025-10-02 20:26:05 -0500

    Delete battery-fleet-solutions-kc directory

--- battery-fleet-solutions-kc/js/main.js
+++
@@ -1,9 +0,0 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-if (navToggle && navLinks) {
-  navToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-    navToggle.setAttribute('aria-expanded', navLinks.classList.contains('open'));
-  });
-}
